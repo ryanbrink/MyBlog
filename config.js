3f069f76d68bc6a81153ce9fa50b934153c40095
Ryan Brink <ryanbrink@Ryans-MacBook-Pro-7.local>
2014-08-14 19:20:10 -0400

    Updating production config

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ config = {
     // ### Development **(default)**
     development: {
         // The url to use when providing links to the site, E.g. in RSS and email.
-        url: 'http://my-ghost-blog.com',
+        url: 'http://ryanbrink.com',
 
         // Example mail config
         // Visit http://support.ghost.org/mail for instructions
@@ -45,7 +45,7 @@ config = {
             // Host to be passed to node's `net.Server#listen()`
             host: '0.0.0.0',
             // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
-            port: process.env.PORT
+            port: '9999'
         },
         paths: {
             contentPath: path.join(__dirname, '/content/')
@@ -75,9 +75,9 @@ config = {
         },
         server: {
             // Host to be passed to node's `net.Server#listen()`
-            host: '127.0.0.1',
+            host: '0.0.0.0',
             // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
-            port: '2368'
+            port: process.env.PORT
         }
     },
 
