19d11416056575d6dc7a54ed320e8d2efbdf3c5b
Ryan Brink <ryanbrink@Ryans-MacBook-Pro.local>
2016-03-05 12:28:22 -0500

    Redirects to new site

--- core/server/routes/frontend.js
+++ core/server/routes/frontend.js
@@ -27,6 +27,38 @@ frontendRoutes = function () {
         res.redirect(subdir + '/ghost/');
     });
 
+    router.get('*digitally-mixing-modern-worship-part-3-life-with-the-gld-80*', function(req, res, next) {
+        res.redirect(301, 'http://www.rythechurchtechguy.com/digitally-mixing-modern-worship-part-3-life-with-the-gld-80/');
+    });
+
+    router.get('*digitally-mixing-modern-worship-part-2-rethinking-everything-with-dante*', function(req, res, next) {
+        res.redirect(301, 'http://www.rythechurchtechguy.com/digitally-mixing-modern-worship-part-2-rethinking-everything-with-dante/');
+    });
+
+    router.get('*digitally-mixing-modern-worship-part-1-getting-started-with-the-gld-80*', function(req, res, next) {
+        res.redirect(301, 'http://www.rythechurchtechguy.com/digitally-mixing-modern-worship-part-1-getting-started-with-the-gld-80/');
+    });
+
+    router.get('*worship-synth-mainstage-keyboard-setup*', function(req, res, next) {
+        res.redirect(301, 'http://www.rythechurchtechguy.com/worship-synth-mainstage-keyboard-setup/');
+    });
+
+    router.get('*performance-drumsloops-setup-for-june-rocks*', function(req, res, next) {
+        res.redirect(301, 'http://www.rythechurchtechguy.com/performance-drumsloops-setup-for-june-rocks/');
+    });
+
+    router.get('*live-music-part-2-audio-and-midi-routing*', function(req, res, next) {
+        res.redirect(301, 'http://www.rythechurchtechguy.com/live-music-part-2-audio-and-midi-routing/');
+    });
+
+    router.get('*live-music-part-1-hardware*', function(req, res, next) {
+        res.redirect(301, 'http://www.rythechurchtechguy.com/live-music-part-1-hardware/');
+    });
+    
+
+
+
+
     // ### Frontend routes
     router.get('/rss/', frontend.rss);
     router.get('/rss/:page/', frontend.rss);
@@ -46,7 +78,7 @@ frontendRoutes = function () {
     router.get('/author/:slug/rss/', frontend.rss);
     router.get('/author/:slug/rss/:page/', frontend.rss);
     router.get('/author/:slug/page/:page/', frontend.author);
-    router.get('/author/:slug/', frontend.author);
+    router.get('/author/:slug/', frontend.author);    
     
     router.get('/gone-hunting', function(req, res, next){
         res.render('ga-today-ph');
@@ -86,7 +118,21 @@ frontendRoutes = function () {
 
     // Default
     router.get('/page/:page/', frontend.homepage);
-    router.get('/', frontend.homepage);
+    router.get('/', function(req, res, next){
+        res.render('landing');
+      });
+
+    router.get('/leadership', function(req, res, next) {
+        frontend.custom_page(req, res, next, "leadership")
+    });
+    router.get('/music', function(req, res, next) {
+        frontend.custom_page(req, res, next, "music")
+    });
+    router.get('/programming', function(req, res, next) {
+        frontend.custom_page(req, res, next, "programming")
+    });
+    
+    router.get('/blog', frontend.homepage);
     router.get('*', frontend.single);
 
     return router;
